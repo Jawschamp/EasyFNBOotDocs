4fe88cefab24d7586a74db6307665fc085eb9e03
Jawschamp <38185563+Jawschamp@users.noreply.github.com>
2020-01-17 16:11:19 -0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -30,7 +30,13 @@ release = '0.0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc', 
+    'sphinx.ext.coverage', 
+    'sphinx.ext.napoleon',
+    'sphinx.ext.intersphinx',
+    'sphinxcontrib_trio'
+]
 
 
 autodoc_member_order = 'bysource'
